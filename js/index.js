f90a18ff52cae5ab0ba8fd70db820212172312b0
wowappswow <wowappswow@gmail.com>
2020-12-01 21:15:00 +0800

    1. 2020/12/01 schedule 2. Finish enter page

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const bannerItems = [...document.querySelectorAll('[data-item]')];
+const enterPageBody = document.getElementById('enter-page');
+
+enterPageBody.onload = pageLoading;
+
+function  pageLoading(){
+    console.log('Page load success.');
+    bannerAnimation();
+}
+
+function bannerAnimation(){
+    for(let item of bannerItems){
+        bannerAnimateClassAdd(item);
+        if(item.dataset.item === "3"){
+            btnTransitionRecover(item);
+        }
+    }
+}
+
+function bannerAnimateClassAdd(item) {
+    setTimeout(() => {
+        item.classList.add('show');
+        console.log(item.dataset.item);
+    }, item.dataset.item*500);
+}
+
+function btnTransitionRecover(item){
+    setTimeout(() => {
+        item.style.transition = 'all 0.25s ease-out';
+    }, 2000);
+}
